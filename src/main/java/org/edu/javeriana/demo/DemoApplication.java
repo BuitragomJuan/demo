962ba0971ab482db481005c37d80e5b93ca1aa21
Juan Buitrago <buitragomjuan@outlook.com>
2023-08-24 19:08:23 -0500

    push inicial en el repositorio, estructura

---
+++ src/main/java/org/edu/javeriana/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package org.edu.javeriana.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
